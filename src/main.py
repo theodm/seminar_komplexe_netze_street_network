f108c0e5d35486feda29c4d310625587c5ca2614
Theodor Diesner-Mayer <theodm@users.noreply.github.com>
2023-11-27 14:00:13 +0000

    Experimente mit graph_too (deutlich schneller)

--- src/main.py
+++ src/main.py
@@ -12,18 +12,20 @@ memory = Memory("cachedir", verbose=0)
 def cached_graph_from_place(place, network_type):
     return ox.graph_from_place(place, network_type=network_type)
 
-G = cached_graph_from_place("Frauenstein, Germany", network_type="drive")
+# G = cached_graph_from_place("Hessen, Deutschland", network_type="drive")
 
-# @memory.cache
-# def cached_graph_from_bbox(north, south, east, west, network_type):
-#     return ox.graph_from_bbox(north, south, east, west, network_type=network_type)
-#
+@memory.cache
+def cached_graph_from_bbox(north, south, east, west, network_type):
+    return ox.graph_from_bbox(north, south, east, west, network_type=network_type)
+
 # G = cached_graph_from_bbox(50.1943, 49.9247, 8.4011, 8.0828, network_type="drive")
+
+# 50.2604 49.8761 9.0088 7.9308
+G = cached_graph_from_bbox(50.2604, 49.8761, 9.0088, 7.9308, network_type="drive")
 
 G = ox.speed.add_edge_speeds(G)
 G = ox.speed.add_edge_travel_times(G)
-
-fig, ax = ox.plot_graph(G)
+print("0")
 
 print("1")
 
@@ -37,7 +39,35 @@ def cached_edge_betweeness(G, weight):
     # ec = ox.plot.get_edge_colors_by_attr(G, "edge_centrality", cmap="inferno")
     # fig, ax = ox.plot_graph(G, edge_color=ec, edge_linewidth=2, node_size=0)
 
-edge_betweenness_centrality = cached_edge_betweeness(G, "travel_time")
+import pyintergraph
+import graph_tool
+
+for e in G.edges:
+    G.edges[e]["edge_label"] = str(e)
+
+graph_tool_graph = pyintergraph.nx2gt(G, "node_label")
+
+
+vb, eb = graph_tool.centrality.betweenness(graph_tool_graph)
+print(2)
+edge_betweenness_centrality = {}
+
+# for v in graph_tool_graph.vertices():
+#     print(graph_tool_graph.vertex_properties["node_label"][v])
+#     print(vb[v])
+
+for e in graph_tool_graph.edges():
+    # convert edge label back to tuple from regex (u, v, x)
+    # dirty hack
+    edge = eval(graph_tool_graph.edge_properties["edge_label"][e])
+
+    edge_betweenness_centrality[edge] = eb[e]
+    
+
+
+
+
+#edge_betweenness_centrality = cached_edge_betweeness(G, "travel_time")
 
 def modify_v(x):
     # log(x+1)
@@ -45,23 +75,22 @@ def modify_v(x):
 
 edge_betweenness_centrality = {(k[0], k[1], 0): modify_v(v) for k, v in edge_betweenness_centrality.items()}
 
+
 # Es werden nicht alle Kanten von edge_betweenness_centrality zurückgegeben,
 # wir suchen die fehlenden Kanten und fügen sie mit dem Wert 0 hinzu
 for edge in G.edges:
     if edge not in edge_betweenness_centrality.keys():
-        print("?")
-        print(edge)
         edge_betweenness_centrality[edge] = 0
 
         nx.set_edge_attributes(
             G, edge_betweenness_centrality, "edge_betweenness_centrality")
-        print("2")
 
 ec = ox.plot.get_edge_colors_by_attr(
     G, "edge_betweenness_centrality", cmap="inferno")
 
 fig, ax = ox.plot_graph(
-    G, edge_color=ec, edge_linewidth=2, node_size=0, dpi=5000, figsize=(30, 30))
+    G, edge_color=ec, edge_linewidth=2, node_size=0, dpi=50, figsize=(30, 30), filepath="test.png", save=True)
+
 print("3")
 
 # %%
